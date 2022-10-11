40dfc429c9e003eb0695ec82059b6df2a4f087c0
thdtjdgml415 <thdtjdgml415@naver.com>
2022-10-11 19:26:39 +0900

    2022-10-11

--- htdocs/create/createMember.php
+++ htdocs/create/createMember.php
@@ -4,10 +4,10 @@
     $sql = "CREATE TABLE myMember (";
     $sql .= " myMemberID int(10) unsigned Not NULL auto_increment,";
     $sql .= " youEmail varchar(40) NOT NULL,";
-    $sql .= "youName varchar(10) NOT NULL,";
+    $sql .= " youName varchar(10) NOT NULL,";
     $sql .= " youPass varchar(40) NOT NULL,";
     $sql .= " youNickName varchar(40) NOT NULL,";
-    $sql .= "youPhone varchar(20) NOT NULL,";
+    $sql .= " youPhone varchar(20) NOT NULL,";
     $sql .= " regTime int(20) NOT NULL,";
     $sql .= " PRIMARY KEY (myMemberID)";
     $sql .= ")charset=utf8;";
--- htdocs/include/footer.php
+++ htdocs/include/footer.php
@@ -2,6 +2,10 @@
         <h2>푸터 영역입니다.</h2>
         <div class="footer__inner container">
             <address>Copyright @2022 SONG</address>
-            <div>gogogogogogoo</div>
+            <div>gogogogogogoo |
+
+                <a href="../admin/adminJoin.php">관리자 로그인</a>
+
+            </div>
         </div>
     </footer>
